5578bc047d5f601bc6c31e24c95f2ef5b767ec95
ErolB <eroljb@hotmail.com>
2017-09-05 23:10:40 -0400

    queries

--- queries.js
+++ queries.js
@@ -1,17 +1,27 @@
 /* Fill out these functions using Mongoose queries*/
+var mongoose = require('mongoose');
+var Listing = require('./ListingSchema');
+var config = require('./config.js');
+
+// set up database connection
+mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.find({name: 'Library West'}, function(error, data){
+        console.log(data);
+  })
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+  Listing.findOneAndRemove({code: 'CABL'}, function(error, data){});
 };
 var updatePhelpsLab = function() {
   /*
@@ -20,9 +30,9 @@ var updatePhelpsLab = function() {
    */
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+  Listing.find({}, function(error, data){
+      console.log(data);
+  })
 };
 
 findLibraryWest();
