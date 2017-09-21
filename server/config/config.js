1e6a8e726c5307151c3d9b12c310e8f623656794
ErolB <eroljb@hotmail.com>
2017-09-20 21:37:46 -0400

    config

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+        uri: 'mongodb://root:password@ds133964.mlab.com:33964/cen3031', //place the URI of your mongo database here.
+    },
+    googleMaps: {
+        key: ''
+    },
+    port: 8080
+};
